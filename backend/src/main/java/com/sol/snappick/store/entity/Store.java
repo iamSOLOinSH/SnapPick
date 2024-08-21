5b6ddc3591f857f93c9cd5a5c3d2eaa45b1484d4
김인엽 <inyub98@gmail.com>
2024-08-21 15:52:36 +0900

    feat: delete unnecessary annotation

--- backend/src/main/java/com/sol/snappick/store/entity/Store.java
+++ backend/src/main/java/com/sol/snappick/store/entity/Store.java
@@ -17,11 +17,9 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity
 @Getter
-@Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Store extends BaseEntity {
 
