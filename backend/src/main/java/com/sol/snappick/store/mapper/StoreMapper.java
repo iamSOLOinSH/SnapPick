9956335d8980d11d044f1f7f503c40cf14fb9986
김인엽 <inyub98@gmail.com>
2024-08-21 16:30:10 +0900

    feat : init store mapper code

--- backend/src/main/java/com/sol/snappick/store/mapper/StoreMapper.java
+++ backend/src/main/java/com/sol/snappick/store/mapper/StoreMapper.java
@@ -5,18 +5,4 @@ import org.mapstruct.Mapper;
 @Mapper(componentModel = "spring")
 public interface StoreMapper {
 
-//    @Mapping(target = "tags", ignore = true)
-//    Store toEntity(StoreCreateReq dto);
-//
-//    @Mapping(source = "entity.tags", target = "tags")
-//    StoreRes toDto(Store entity);
-//
-//    default List<String> mapTags(List<StoreTag> tags) {
-//        return tags.stream()
-//                   .map(StoreTag::getTag)
-//                   .collect(Collectors.toList());
-//    }
-//    default List<StoreTag> mapTagStringsToEntities(List<String> tags, @MappingTarget Store store) {
-//        return tags.stream().map(tag -> StoreTag.builder())
-//    }
 }
