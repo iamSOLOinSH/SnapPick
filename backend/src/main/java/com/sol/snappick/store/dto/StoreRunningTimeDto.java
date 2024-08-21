1a0333859dc3fbe313a3d3a1960be6761c475cf2
김인엽 <inyub98@gmail.com>
2024-08-22 00:50:23 +0900

    feat : add constructor with builder, LocalTime -> String - 이유 : localtime이 object 형식으로 받게 됨

--- backend/src/main/java/com/sol/snappick/store/dto/StoreRunningTimeDto.java
+++ backend/src/main/java/com/sol/snappick/store/dto/StoreRunningTimeDto.java
@@ -2,18 +2,32 @@ package com.sol.snappick.store.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.DayOfWeek;
-import java.time.LocalTime;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-@Builder
 @Getter
-@Schema(description = "스토어 ")
+@NoArgsConstructor
+@Schema(description = "스토어 운영시간 DTO")
 public class StoreRunningTimeDto {
 
+    @Schema(description = "요일")
     private DayOfWeek dayOfWeek;
 
-    private LocalTime startTime;
+    @Schema(description = "요일별 운영 시작 시간")
+    private String startTime;
 
-    private LocalTime endTime;
+    @Schema(description = "요일별 운영 종료 시간")
+    private String endTime;
+
+    @Builder
+    public StoreRunningTimeDto(
+        DayOfWeek dayOfWeek,
+        String startTime,
+        String endTime
+    ) {
+        this.dayOfWeek = dayOfWeek;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
 }
