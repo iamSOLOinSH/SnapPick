81830a5588891727756f279fe78ec45694093615
김인엽 <inyub98@gmail.com>
2024-08-21 15:55:42 +0900

    feat: add store running time repo

---
+++ backend/src/main/java/com/sol/snappick/store/repository/StoreRunningTimeRepository.java
@@ -0,0 +1,8 @@
+package com.sol.snappick.store.repository;
+
+import com.sol.snappick.store.entity.StoreRunningTime;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StoreRunningTimeRepository extends JpaRepository<StoreRunningTime, Long> {
+
+}
