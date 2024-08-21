024516fc21419c9a97cc1c697a7b942a50547f62
김인엽 <inyub98@gmail.com>
2024-08-21 15:55:31 +0900

    feat: add store image repo

---
+++ backend/src/main/java/com/sol/snappick/store/repository/StoreImageRepository.java
@@ -0,0 +1,8 @@
+package com.sol.snappick.store.repository;
+
+import com.sol.snappick.store.entity.StoreImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StoreImageRepository extends JpaRepository<StoreImage, Integer> {
+
+}
