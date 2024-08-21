95ee3beb7e07365217f193bfed8fda604cf27481
김인엽 <inyub98@gmail.com>
2024-08-21 15:55:58 +0900

    feat: add store tag repo

---
+++ backend/src/main/java/com/sol/snappick/store/repository/StoreTagRepository.java
@@ -0,0 +1,8 @@
+package com.sol.snappick.store.repository;
+
+import com.sol.snappick.store.entity.StoreTag;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StoreTagRepository extends JpaRepository<StoreTag, Integer> {
+
+}
