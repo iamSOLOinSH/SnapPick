0ba87da100671398c3c5caa6c0443191e4575b87
q0_u_0p <q0_u_0p@naver.com>
2024-08-22 16:39:08 +0900

    feat: add config for expression
    - dto in dto expression to Request

--- backend/src/main/java/com/sol/snappick/global/SwaggerConfig.java
+++ backend/src/main/java/com/sol/snappick/global/SwaggerConfig.java
@@ -3,16 +3,26 @@ package com.sol.snappick.global;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
 
+
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI().info(new Info().title("Snappick API")
-                                            .version("1.0")
-                                            .description("Snappick API Documentation"));
+                .version("1.0")
+                .description("Snappick API Documentation"));
+    }
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public")
+                .pathsToMatch("/**")
+                .build();
     }
 }
