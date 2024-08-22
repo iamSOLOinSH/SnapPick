c45119fe7729bf282f5032a3b68d7e7ed9af057b
김인엽 <inyub98@gmail.com>
2024-08-22 10:58:28 +0900

    feat: swagger version up 으로 인한 변경

--- backend/src/main/java/com/sol/snappick/global/FrontController.java
+++ backend/src/main/java/com/sol/snappick/global/FrontController.java
@@ -1,5 +1,6 @@
 package com.sol.snappick.global;
 
+import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class FrontController {
 
-    
+    @Hidden
     @GetMapping
     public String index() {
         return "redirect:/swagger-ui/index.html";
