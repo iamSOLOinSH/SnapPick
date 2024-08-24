42d96c04511a59f9fda49ea01fb98c83a8ad06d8
김인엽 <inyub98@gmail.com>
2024-08-24 22:45:05 +0900

    refactor : swagger docs update

--- backend/src/main/java/com/sol/snappick/store/controller/StoreController.java
+++ backend/src/main/java/com/sol/snappick/store/controller/StoreController.java
@@ -22,7 +22,46 @@ public class StoreController {
     public final StoreService storeService;
 
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    @Operation(summary = "팝업 스토어 등록", description = "팝업 스토어 등록 API")
+    @Operation(summary = "팝업 스토어 등록", description = """
+        팝업 스토어 등록
+        \n
+        \n
+        <b>Input</b>:
+        \n
+        <b>StoreCreateReq</b>
+        | Name | Type  | Description |
+        |-----|-----|-------|
+        | name | string | 스토어 이름 |
+        | description | string | 스토어 설명 |
+        | location | string | 스토어 위치 |
+        | latitude | double | 스토어 위치의 위도 |
+        | longitude | double | 스토어 위치의 경도 |
+        | operateStartAt | LocalDate | 운영 시작 날짜 |
+        | operateEndAt | LocalDate | 운영 종료 날짜 |
+        | sellerId | integer | 판매자 ID |
+        | tags | list(String) | 태그 목록 |
+        | images | list(StoreImageDto) | 스토어 이미지 목록. <b>여기는 비워서 주세요!</b> |
+        | runningTimes | list(StoreRunningTimeDto) | 운영 시간 목록 |
+        \n
+        \n
+        <b>StoreRunningTimeDto</b>
+        | Name | Type  | Description |
+        |-----|-----|-------|
+        | dayOfWeek | DayOfWeek(Enum) | MONDAY,TUESDAY,... 영어 대문자! |
+        | startTime | string | 시작 시간 ex) 10:00 |
+        | endTime | string | 종료 시간 ex) 16:00 |
+        <br>
+        <br>
+        여기에 추가로 images 넣어주시면 됩니다!
+                
+        <b>Output</b>:
+        <br>
+            type: _description_
+
+        | Var | Type | Description |
+        |-----|-----|-------|
+        |  |  |  |
+        """)
     public ResponseEntity<StoreRes> createPopupStore(
         @RequestPart("storeCreateReq") StoreCreateReq storeCreateReq,
         @RequestPart(value = "images", required = false) MultipartFile[] images
@@ -33,8 +72,10 @@ public class StoreController {
     }
 
     @PostMapping("/init")
+    @Operation(summary = "스토어 API 에서 데이터 가져오는 것! 한 번만 누르기")
     public ResponseEntity<HttpStatus> postInitData() throws Exception {
         storeService.postInitData();
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+        return ResponseEntity.status(HttpStatus.CREATED)
+                             .build();
     }
 }
