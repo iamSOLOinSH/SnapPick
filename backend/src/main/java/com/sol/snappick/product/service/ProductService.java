11c158523c35a14f4ac2f1d15d2d3b278c0aba8c
trankill_Kim <73770924+trankill1127@users.noreply.github.com>
2024-08-26 01:17:54 +0900

    feat: add createProduct in productService

--- backend/src/main/java/com/sol/snappick/product/service/ProductService.java
+++ backend/src/main/java/com/sol/snappick/product/service/ProductService.java
@@ -1,18 +1,113 @@
 package com.sol.snappick.product.service;
 
+import com.sol.snappick.product.dto.ProductCreateReq;
+import com.sol.snappick.product.dto.ProductDetailRes;
+import com.sol.snappick.product.entity.Product;
+import com.sol.snappick.product.entity.ProductImage;
+import com.sol.snappick.product.entity.ProductOption;
+import com.sol.snappick.product.exception.ProductImageLimitExceedException;
+import com.sol.snappick.store.exception.StoreNotFoundException ;
+import com.sol.snappick.product.mapper.ProductImageMapper;
+import com.sol.snappick.product.mapper.ProductMapper;
+import com.sol.snappick.product.mapper.ProductOptionMapper;
 import com.sol.snappick.product.repository.*;
+import com.sol.snappick.store.entity.Store;
+import com.sol.snappick.store.repository.StoreRepository;
+import com.sol.snappick.util.ImageUploadRes;
+import com.sol.snappick.util.MinioUtil;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class ProductService {
 
-    private final CartItemRepository cartItemRepository;
-    private final CartRepository cartRepository;
     private final ProductImageRepository productImageRepository;
     private final ProductOptionRepository productOptionRepository;
     private final ProductRepository productRepository;
+    private final StoreRepository storeRepository;
 
+    private final ProductMapper productMapper;
+    private final ProductImageMapper productImageMapper;
+    private final ProductOptionMapper productOptionMapper;
+
+    private final String BUCKET_NAME = "snappick-product";
+    private final MinioUtil minioUtil;
+
+    @Transactional
+    public ProductDetailRes createProduct(
+            Integer storeId,
+            ProductCreateReq productCreateReq,
+            MultipartFile[] images
+    ) throws Exception{
+
+        // 유효성 검증
+        // 1) 팝업스토어
+        if (!storeRepository.existsById(storeId)){
+            throw new StoreNotFoundExcpetion();
+        }
+
+        // 2) 이미지
+        if (images!=null && images.length>10){
+            throw new ProductImageLimitExceedException();
+        }
+
+        //Product 엔티티를 생성한다.
+        Product productToCreate = productMapper.toEntity(productCreateReq);
+        productToCreate = productRepository.save(productToCreate);
+
+        //Store
+        Optional<Store> storeResponse = storeRepository.findById(storeId);
+        productToCreate.setStore(storeResponse.get());
+
+        //이미지 처리 및 저장
+        if (images!=null){
+            List<ProductImage> productImages = uploadImagesToMinio(images, productToCreate);
+            productImageRepository.saveAll(productImages);
+            productToCreate.setImages(productImages);
+        }
+
+        //옵션 처리
+        if (productCreateReq.getOptions()!=null){
+            List<ProductOption> productOptions = productOptionMapper.toEntityList(productCreateReq.getOptions(), productToCreate);
+            productOptionRepository.saveAll(productOptions);
+            productToCreate.setOptions(productOptions);
+        }
+
+        //최종 DTO로 젼환하여 반환
+        return productMapper.toDetailDto(productToCreate);
+    }
+
+    private List<ProductImage> uploadImagesToMinio(
+            MultipartFile[] images,
+            Product product
+    ) throws Exception {
+        List<ProductImage> productImages = new ArrayList<>();
+
+        for (MultipartFile image : images) {
+            if (image.isEmpty()) {
+                continue;
+            }
+
+            ImageUploadRes imageDto = minioUtil.uploadImageWithThumbnail(
+                    BUCKET_NAME, image); // 이미지 저장 메서드 필요
+            if (imageDto == null) {
+                continue;
+            }
+            ProductImage productImage = ProductImage.builder()
+                    .originImageUrl(imageDto.getOriginImageUrl())
+                    .thumbnailImageUrl(imageDto.getThumbnailImageUrl())
+                    .product(product)
+                    .build();
+            productImages.add(productImage);
+        }
+        return productImages;
+    }
 
 }
