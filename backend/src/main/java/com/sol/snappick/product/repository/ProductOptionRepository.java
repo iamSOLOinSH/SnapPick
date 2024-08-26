d6406085af12cfa0d004f07c47d09acee3f6a027
trankill_Kim <73770924+trankill1127@users.noreply.github.com>
2024-08-26 20:17:40 +0900

    feat: modify ProductOptionRepository

--- backend/src/main/java/com/sol/snappick/product/repository/ProductOptionRepository.java
+++ backend/src/main/java/com/sol/snappick/product/repository/ProductOptionRepository.java
@@ -1,7 +1,8 @@
 package com.sol.snappick.product.repository;
 
 import com.sol.snappick.product.entity.Product;
+import com.sol.snappick.product.entity.ProductOption;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductOptionRepository extends JpaRepository<Product, Integer> {
+public interface ProductOptionRepository extends JpaRepository<ProductOption, Integer> {
 }
