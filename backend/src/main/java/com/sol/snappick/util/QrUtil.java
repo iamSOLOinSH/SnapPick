4316506b7fd1850ac4423b08e61b3c634e4bbd77
김인엽 <inyub98@gmail.com>
2024-08-26 21:42:25 +0900

    feat: add qr util

---
+++ backend/src/main/java/com/sol/snappick/util/QrUtil.java
@@ -0,0 +1,34 @@
+package com.sol.snappick.util;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.stereotype.Service;
+
+public class QrUtil {
+
+    public static byte[] generateQrCode(String text, int width, int height) throws IOException {
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        Map<EncodeHintType, ErrorCorrectionLevel> hints = new HashMap<>();
+        hints.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H);
+
+        try {
+            BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height, hints);
+            ByteArrayOutputStream pngOutputStream = new ByteArrayOutputStream();
+            MatrixToImageWriter.writeToStream(bitMatrix, "PNG", pngOutputStream);
+            return pngOutputStream.toByteArray();
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new IOException("QR 코드 생성 중 오류가 발생했습니다.", e);
+        }
+    }
+}
