37c5c68fd059f0a554014e474753bd0698dbaf0e
김인엽 <inyub98@gmail.com>
2024-08-27 14:44:28 +0900

    feat: add exception and handler for member and product

---
+++ backend/src/main/java/com/sol/snappick/member/exception/MemberNotFoundException.java
@@ -0,0 +1,12 @@
+package com.sol.snappick.member.exception;
+
+public class MemberNotFoundException extends RuntimeException {
+
+    public MemberNotFoundException() {
+        super("사용자를 찾을 수 없습니다.");
+    }
+
+    public MemberNotFoundException(String message) {
+        super(message);
+    }
+}
