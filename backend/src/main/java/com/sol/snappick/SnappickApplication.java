45cbbe12d96ed2db7093490c53aec48b525add91
q0_u_0p <q0_u_0p@naver.com>
2024-08-27 10:32:11 +0900

    fix: add server to swagger for CORS
    - request url(http, https) problem

--- backend/src/main/java/com/sol/snappick/SnappickApplication.java
+++ backend/src/main/java/com/sol/snappick/SnappickApplication.java
@@ -1,13 +1,18 @@
 package com.sol.snappick;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers =
+        {@Server(url = "/",
+                description = "Default Server URL")})
 @SpringBootApplication
 public class SnappickApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SnappickApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SnappickApplication.class, args);
+    }
 
 }
