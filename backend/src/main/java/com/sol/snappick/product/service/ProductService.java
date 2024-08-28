e58794a0c4ccff2e8f673d94ea5665c12f3e96b4
김인엽 <inyub98@gmail.com>
2024-08-28 19:56:54 +0900

    feat : minio package location change

--- backend/src/main/java/com/sol/snappick/product/service/ProductService.java
+++ backend/src/main/java/com/sol/snappick/product/service/ProductService.java
@@ -1,5 +1,11 @@
 package com.sol.snappick.product.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
 import com.sol.snappick.product.dto.ProductCreateReq;
 import com.sol.snappick.product.dto.ProductDetailRes;
 import com.sol.snappick.product.dto.ProductSimpleRes;
@@ -7,175 +13,177 @@ import com.sol.snappick.product.entity.Product;
 import com.sol.snappick.product.entity.ProductImage;
 import com.sol.snappick.product.exception.ProductImageLimitExceedException;
 import com.sol.snappick.product.exception.ProductNotFoundException;
-import com.sol.snappick.store.exception.StoreNotFoundException ;
 import com.sol.snappick.product.mapper.ProductMapper;
-import com.sol.snappick.product.repository.*;
+import com.sol.snappick.product.repository.ProductImageRepository;
+import com.sol.snappick.product.repository.ProductRepository;
 import com.sol.snappick.store.entity.Store;
+import com.sol.snappick.store.exception.StoreNotFoundException;
 import com.sol.snappick.store.repository.StoreRepository;
-import com.sol.snappick.util.ImageUploadRes;
-import com.sol.snappick.util.MinioUtil;
+import com.sol.snappick.util.minio.ImageUploadRes;
+import com.sol.snappick.util.minio.MinioUtil;
+
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ProductService {
 
-    private final ProductImageRepository productImageRepository;
-    private final ProductRepository productRepository;
-    private final StoreRepository storeRepository;
+	private final ProductImageRepository productImageRepository;
+	private final ProductRepository productRepository;
+	private final StoreRepository storeRepository;
 
-    private final ProductMapper productMapper;
+	private final ProductMapper productMapper;
 
-    private final String BUCKET_NAME = "snappick-product";
-    private final MinioUtil minioUtil;
+	private final String BUCKET_NAME = "snappick-product";
+	private final MinioUtil minioUtil;
 
-    @Transactional
-    public ProductDetailRes createProduct(
-            Integer storeId,
-            ProductCreateReq productCreateReq,
-            MultipartFile[] images
-    ) throws Exception{
+	@Transactional
+	public ProductDetailRes createProduct(
+			Integer storeId,
+			ProductCreateReq productCreateReq,
+			MultipartFile[] images
+	) throws Exception {
 
-        // 유효성 검증
-        // 1) 팝업스토어
-        Store store = storeRepository.findById(storeId)
-                .orElseThrow(() -> new StoreNotFoundException());
+		// 유효성 검증
+		// 1) 팝업스토어
+		Store store = storeRepository.findById(storeId)
+									 .orElseThrow(() -> new StoreNotFoundException());
 
-        // 2) 이미지
-        if (images!=null && images.length>10){
-            throw new ProductImageLimitExceedException();
-        }
+		// 2) 이미지
+		if ( images != null && images.length > 10 ) {
+			throw new ProductImageLimitExceedException();
+		}
 
-        //Product 엔티티를 생성한다.
-        Product productToCreate = productMapper.toEntity(productCreateReq);
-        productToCreate = productRepository.save(productToCreate);
+		//Product 엔티티를 생성한다.
+		Product productToCreate = productMapper.toEntity(productCreateReq);
+		productToCreate = productRepository.save(productToCreate);
 
-        //Store
-        productToCreate.setStore(store);
+		//Store
+		productToCreate.setStore(store);
 
-        //이미지 처리 및 저장
-        if (images!=null){
-            List<ProductImage> productImages = uploadImagesToMinio(images, productToCreate);
-            productImageRepository.saveAll(productImages);
-            productToCreate.setImages(productImages);
-        }
+		//이미지 처리 및 저장
+		if ( images != null ) {
+			List<ProductImage> productImages = uploadImagesToMinio(images,
+																   productToCreate);
+			productImageRepository.saveAll(productImages);
+			productToCreate.setImages(productImages);
+		}
 
-        //최종 DTO로 젼환하여 반환
-        return productMapper.toDetailDto(productToCreate);
-    }
+		//최종 DTO로 젼환하여 반환
+		return productMapper.toDetailDto(productToCreate);
+	}
 
-    @Transactional
-    public ProductDetailRes readProduct(
-            Integer productId
-    ) throws Exception{
+	@Transactional
+	public ProductDetailRes readProduct(
+			Integer productId
+	) throws Exception {
 
-        Product product = productRepository.findById(productId)
-                .orElseThrow(() -> new ProductNotFoundException());
+		Product product = productRepository.findById(productId)
+										   .orElseThrow(() -> new ProductNotFoundException());
 
-        return productMapper.toDetailDto(product);
-    }
+		return productMapper.toDetailDto(product);
+	}
 
-    @Transactional
-    public List<ProductSimpleRes> readStoreProducts(Integer storeId)
-            throws Exception{
+	@Transactional
+	public List<ProductSimpleRes> readStoreProducts(Integer storeId) throws Exception {
 
-        Store store = storeRepository.findById(storeId)
-                .orElseThrow(() -> new StoreNotFoundException());
+		Store store = storeRepository.findById(storeId)
+									 .orElseThrow(() -> new StoreNotFoundException());
 
-        return productRepository.findByStore(store).stream()
-                .map(productMapper::toSimpleDto)
-                .toList();
-    }
+		return productRepository.findByStore(store)
+								.stream()
+								.map(productMapper::toSimpleDto)
+								.toList();
+	}
 
-    @Transactional
-    public ProductDetailRes updateProduct(Integer productId, ProductCreateReq productCreateReq, MultipartFile[] images) throws Exception {
-        Product productToUpdate = productRepository.findById(productId)
-                .orElseThrow(() -> new ProductNotFoundException());
+	@Transactional
+	public ProductDetailRes updateProduct(
+			Integer productId,
+			ProductCreateReq productCreateReq,
+			MultipartFile[] images
+	) throws Exception {
+		Product productToUpdate = productRepository.findById(productId)
+												   .orElseThrow(() -> new ProductNotFoundException());
 
-        productToUpdate.updateDetails(
-                productCreateReq.getName(),
-                productCreateReq.getDescription(),
-                productCreateReq.getPrice(),
-                productCreateReq.getStock(),
-                productCreateReq.getDailyLimit(),
-                productCreateReq.getPersonalLimit()
-        );
+		productToUpdate.updateDetails(productCreateReq.getName(),
+									  productCreateReq.getDescription(),
+									  productCreateReq.getPrice(),
+									  productCreateReq.getStock(),
+									  productCreateReq.getDailyLimit(),
+									  productCreateReq.getPersonalLimit());
 
-        // 이미지 처리
-        if (images != null && images.length > 0) {
-            // 기존 이미지 삭제
-            for (ProductImage image : new ArrayList<>(productToUpdate.getImages())) {
-                minioUtil.deleteImage(image.getOriginImageUrl());
-                minioUtil.deleteImage(image.getThumbnailImageUrl());
-                productToUpdate.getImages().remove(image);
-                productImageRepository.delete(image);
-            }
+		// 이미지 처리
+		if ( images != null && images.length > 0 ) {
+			// 기존 이미지 삭제
+			for (ProductImage image : new ArrayList<>(productToUpdate.getImages())) {
+				minioUtil.deleteImage(image.getOriginImageUrl());
+				minioUtil.deleteImage(image.getThumbnailImageUrl());
+				productToUpdate.getImages()
+							   .remove(image);
+				productImageRepository.delete(image);
+			}
 
-            // 새 이미지 추가
-            List<ProductImage> newImages = uploadImagesToMinio(images, productToUpdate);
-            productToUpdate.getImages().addAll(newImages);
-            productImageRepository.saveAll(newImages);
-        }
+			// 새 이미지 추가
+			List<ProductImage> newImages = uploadImagesToMinio(images,
+															   productToUpdate);
+			productToUpdate.getImages()
+						   .addAll(newImages);
+			productImageRepository.saveAll(newImages);
+		}
 
-        Product updatedProduct = productRepository.save(productToUpdate);
-        return productMapper.toDetailDto(updatedProduct);
-    }
+		Product updatedProduct = productRepository.save(productToUpdate);
+		return productMapper.toDetailDto(updatedProduct);
+	}
 
+	@Transactional
+	public boolean deleteProduct(
+			Integer productId
+	) throws Exception {
 
-    @Transactional
-    public boolean deleteProduct(
-            Integer productId
-    ) throws Exception{
+		Product productToDelete = productRepository.findById(productId)
+												   .orElseThrow(() -> new ProductNotFoundException());
 
-        Product productToDelete = productRepository.findById(productId)
-                .orElseThrow(() -> new ProductNotFoundException());
+		//이미지 삭제
+		for (ProductImage image : productToDelete.getImages()) {
+			minioUtil.deleteImage(image.getOriginImageUrl());
+			minioUtil.deleteImage(image.getThumbnailImageUrl());
+		}
+		productImageRepository.deleteAll(productToDelete.getImages());
 
-        //이미지 삭제
-        for (ProductImage image: productToDelete.getImages()){
-            minioUtil.deleteImage(image.getOriginImageUrl());
-            minioUtil.deleteImage(image.getThumbnailImageUrl());
-        }
-        productImageRepository.deleteAll(productToDelete.getImages());
+		try {
+			productRepository.delete(productToDelete);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
 
-        try {
-            productRepository.delete(productToDelete);
-            return true;
-        } catch(Exception  e){
-            return false;
-        }
+	}
 
-    }
+	private List<ProductImage> uploadImagesToMinio(
+			MultipartFile[] images,
+			Product product
+	) throws Exception {
+		List<ProductImage> productImages = new ArrayList<>();
 
-    private List<ProductImage> uploadImagesToMinio(
-            MultipartFile[] images,
-            Product product
-    ) throws Exception {
-        List<ProductImage> productImages = new ArrayList<>();
+		for (MultipartFile image : images) {
+			if ( image.isEmpty() ) {
+				continue;
+			}
 
-        for (MultipartFile image : images) {
-            if (image.isEmpty()) {
-                continue;
-            }
-
-            ImageUploadRes imageDto = minioUtil.uploadImageWithThumbnail(
-                    BUCKET_NAME, image); // 이미지 저장 메서드 필요
-            if (imageDto == null) {
-                continue;
-            }
-            ProductImage productImage = ProductImage.builder()
-                    .originImageUrl(imageDto.getOriginImageUrl())
-                    .thumbnailImageUrl(imageDto.getThumbnailImageUrl())
-                    .product(product)
-                    .build();
-            productImages.add(productImage);
-        }
-        return productImages;
-    }
+			ImageUploadRes imageDto = minioUtil.uploadImageWithThumbnail(BUCKET_NAME,
+																		 image); // 이미지 저장 메서드 필요
+			if ( imageDto == null ) {
+				continue;
+			}
+			ProductImage productImage = ProductImage.builder()
+													.originImageUrl(imageDto.getOriginImageUrl())
+													.thumbnailImageUrl(imageDto.getThumbnailImageUrl())
+													.product(product)
+													.build();
+			productImages.add(productImage);
+		}
+		return productImages;
+	}
 
 }
