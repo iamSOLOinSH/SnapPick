2993f76037cfb6a1c33d38fee5109945167f41ac
q0_u_0p <q0_u_0p@naver.com>
2024-08-29 11:38:17 +0900

    feat: implement my account info get

--- backend/src/main/java/com/sol/snappick/member/dto/AccountStateRes.java
+++ backend/src/main/java/com/sol/snappick/member/dto/AccountStateRes.java
@@ -1,8 +1,12 @@
 package com.sol.snappick.member.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
+@Builder
 public class AccountStateRes {
 
     private String bankName;
--- backend/src/main/java/com/sol/snappick/member/service/TransactionService.java
+++ backend/src/main/java/com/sol/snappick/member/service/TransactionService.java
@@ -1,6 +1,8 @@
 package com.sol.snappick.member.service;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.sol.snappick.member.dto.AccountStateRes;
+import com.sol.snappick.member.entity.Member;
 import com.sol.snappick.util.fin.FinOpenApiHandler;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
@@ -9,7 +11,9 @@ import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -50,6 +54,68 @@ public class TransactionService {
         // 3. 응답값 받아서 반환
         String newAccountNo = jsonNode.get("REC").get("accountNo").textValue();
         return newAccountNo;
+    }
+
+    // 주 계좌 확인하기
+    public AccountStateRes getMyAccount(Integer memberId) {
+        Member member = basicMemberService.getMemberById(memberId);
+
+        String myAccount = member.getAccountNumber();
+        if (myAccount == null) return null;
+
+        // 1. 요청 본문 생성
+        Map<String, Object> requestBody = new HashMap<>();
+        requestBody.put("accountNo", myAccount);
+
+        // 2. api 요청
+        JsonNode jsonNode = finOpenApiHandler.apiRequest("/edu/demandDeposit/inquireDemandDepositAccount", "inquireDemandDepositAccount", HttpMethod.POST, requestBody, member.getUserKey());
+
+        // 3. 응답값 받아서 반환
+        JsonNode responseData = jsonNode.get("REC");
+
+        return AccountStateRes.builder()
+                .bankName(responseData.get("bankName").textValue())
+                .accountNumber(myAccount)
+                .theBalance(Long.valueOf(responseData.get("accountBalance").textValue()))
+                .build();
+
+    }
+
+    // 타행 계좌 확인하기
+    public List<AccountStateRes> getOtherAccount(Integer memberId) {
+        Member member = basicMemberService.getMemberById(memberId);
+
+        String myAccount = member.getAccountNumber();
+
+        // 1. 요청 본문 생성
+        Map<String, Object> requestBody = new HashMap<>();
+
+        // 2. api 요청
+        JsonNode jsonNode = finOpenApiHandler.apiRequest("/edu/demandDeposit/inquireDemandDepositAccountList", "inquireDemandDepositAccountList", HttpMethod.POST, requestBody, member.getUserKey());
+
+        // 3. 응답값 받아서 반환
+        JsonNode responseData = jsonNode.get("REC");
+
+        List<AccountStateRes> accountList = new ArrayList<>();
+        if (!responseData.isArray()) {
+            return accountList;
+        }
+
+        for (JsonNode account : responseData) {
+            if (account.get("accountNo").textValue().equals(myAccount)) {
+                continue;
+            }
+            accountList.add(
+                    AccountStateRes.builder()
+                            .bankName(account.get("bankName").textValue())
+                            .accountNumber(account.get("accountNo").textValue())
+                            .theBalance(Long.valueOf(account.get("accountBalance").textValue()))
+                            .build()
+            );
+        }
+
+
+        return accountList;
     }
 
 
