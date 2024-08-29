82ec555ca23437c2f5abf96d7d009ebf8aea0c6b
q0_u_0p <q0_u_0p@naver.com>
2024-08-29 09:21:54 +0900

    feat: add create account function
    - Allow accountNumber to be null
    - The new seller creates an account automatically

--- backend/src/main/java/com/sol/snappick/member/service/TransactionService.java
+++ backend/src/main/java/com/sol/snappick/member/service/TransactionService.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.sol.snappick.util.fin.FinOpenApiHandler;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,20 +19,35 @@ public class TransactionService {
 
     private final FinOpenApiHandler finOpenApiHandler;
 
+    @Value("${finopenapi.snappickAccount}")
+    private String accountTypeUniqueNo;
+
     // 회원 등록
     @SneakyThrows
     public String postMember(String email) {
 
         // 1. 요청 본문 생성
-        Map<String, String> requestBody = new HashMap<>();
+        Map<String, Object> requestBody = new HashMap<>();
         requestBody.put("userId", email);
 
         // 2. api 요청
-        JsonNode jsonNode = finOpenApiHandler.apiRequest("/member", HttpMethod.POST, requestBody);
+        JsonNode jsonNode = finOpenApiHandler.apiRequest("/member", null, HttpMethod.POST, requestBody, null);
 
         finOpenApiHandler.printJson(jsonNode);
 
         return finOpenApiHandler.getValueByKey(jsonNode, "userKey");
+    }
+
+    // 계좌 생성
+    public String createAccount(String userKey) {
+        // 1. 요청 본문 생성
+        Map<String, Object> requestBody = new HashMap<>();
+        requestBody.put("accountTypeUniqueNo", accountTypeUniqueNo);
+        // 2. api 요청
+        JsonNode jsonNode = finOpenApiHandler.apiRequest("/edu/demandDeposit/inquireDemandDepositAccountList", "inquireDemandDepositAccountList", HttpMethod.POST, requestBody, userKey);
+        finOpenApiHandler.printJson(jsonNode);
+//       getValueByKey테스트해보기
+        return finOpenApiHandler.getValueByKey(jsonNode, "accountNo");
     }
 
 
