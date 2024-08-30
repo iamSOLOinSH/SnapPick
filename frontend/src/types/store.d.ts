4c7ee508267b21306a6904c74bec561f57bb73fb
yunva17 <yunva17@naver.com>
2024-08-30 11:25:26 +0900

    🐛 Fix : 스토어 타입 수정

--- frontend/src/types/store.d.ts
+++ frontend/src/types/store.d.ts
@@ -1,10 +1,33 @@
 export interface Store {
-  id: string;
-  title: string;
-  imageSrc: string;
-  location: string;
-  startDate: string;
-  endDate: string;
+  id: number;
+  name: string;
   description: string;
-  imgScrList: string[];
+  location: string;
+  operateStartAt: string;
+  operateEndAt: string;
+  viewCount: number;
+  visitCount: number;
+  sellerId: null | number;
+  tags: string[];
+  images: ImageObject[];
+  runningTimes: RunningTimesObject[];
+  fromQr: boolean;
+}
+
+interface ImageObject {
+  originImageUrl: string;
+  thumbnailImageUrl: string;
+}
+
+interface RunningTimesObject {
+  dayOfWeek:
+    | "MONDAY"
+    | "TUESDAY"
+    | "WEDNESDAY"
+    | "THURSDAY"
+    | "FRIDAY"
+    | "SATURDAY"
+    | "SUNDAY";
+  startTime: string;
+  endTime: string;
 }
